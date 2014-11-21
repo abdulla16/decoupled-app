1b59358edc43977baca664eb711ded6ff81f50b6
Abdulla Al-Qawasmeh <abdulla16@gmail.com>
2014-11-21 17:28:02 -0600

    Fix issue with GET request

--- Web/js/call_web_service.js
+++ Web/js/call_web_service.js
@@ -1,10 +1,24 @@
 function callService() {
+	var data = null;
+	
+	switch($("#requestType").val().toLowerCase()) {
+		case "get":
+		case "post":
+			data = "data=" + $("#data").val();
+			break;
+		case "put":
+		case "delete":
+			data = $("#data").val();
+			break;
+	}
+	
 	$.ajax({
 		type: $("#requestType").val(),
-		data: $("#data").val(),
+		data: data,
 		url: "/api/" + $("#serviceName").val()
 	}).done(function(result) {
 		
 		$("#results").html(JSON.stringify(result));
 	});
+
 }
